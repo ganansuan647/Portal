7c79d4ff0c7776ce5bdcb1ab0e3c00dd749f01c5
ganansuan647 <1852992@tongji.edu.cn>
2024-09-16 01:00:23 +0800

    修复图片导入问题

--- src/images/MemberPhotos.ts
+++ src/images/MemberPhotos.ts
@@ -1,24 +1,33 @@
-import 崔永嘉证件照 from '@images/成员照片/崔永嘉-证件照.jpg';
-import 卢昊楠证件照 from '@images/成员照片/卢昊楠-证件照.jpg';
-import 周成生活照 from '@images/成员照片/周成-生活照.png';
-import 周成证件照 from '@images/成员照片/周成-证件照.png';
-import 杨光怡生活照 from '@images/成员照片/杨光怡-生活照.jpg';
-import 杨光怡证件照 from '@images/成员照片/杨光怡-证件照.jpg';
-import 苟凌云生活照 from '@images/成员照片/苟凌云-生活照.jpg';
-import 苟凌云生活照2 from '@images/成员照片/苟凌云-生活照2.jpg';
-import 苟凌云证件照 from '@images/成员照片/苟凌云-证件照.jpg';
-import 付凯森证件照 from '@images/成员照片/付凯森-证件照.jpg';
-import 黄泽安然证件照 from '@images/成员照片/黄泽安然-证件照.jpg';
-import 王孟欣证件照 from '@images/成员照片/王孟欣-证件照.jpg';
-import 吴文俊证件照 from '@images/成员照片/吴文俊-证件照.jpg';
-import 杨当证件照 from '@images/成员照片/杨当-证件照.jpg';
-import 叶博兴证件照 from '@images/成员照片/叶博兴-证件照.jpg';
+const CYJ_ZJZ = "@images/成员照片/崔永嘉-证件照.jpg";
+const LHN_ZJZ = "@images/成员照片/卢昊楠-证件照.jpg";
+const ZC_SHZ = "@images/成员照片/周成-生活照.png";
+const ZC_ZJZ = "@images/成员照片/周成-证件照.png";
+const YGY_SHZ = "@images/成员照片/杨光怡-生活照.jpg";
+const YGY_ZJZ = "@images/成员照片/杨光怡-证件照.jpg";
+const GLY_SHZ = "@images/成员照片/苟凌云-生活照.jpg";
+const GLY_SHZ2 = "@images/成员照片/苟凌云-生活照2.jpg";
+const GLY_ZJZ = "@images/成员照片/苟凌云-证件照.jpg";
+const FKS_ZJZ = "@images/成员照片/付凯森-证件照.jpg";
+const HZAR_ZJZ = "@images/成员照片/黄泽安然-证件照.jpg";
+const WMX_ZJZ = "@images/成员照片/王孟欣-证件照.jpg";
+const WWJ_ZJZ = "@images/成员照片/吴文俊-证件照.jpg";
+const YD_ZJZ = "@images/成员照片/杨当-证件照.jpg";
+const YBX_ZJZ = "@images/成员照片/叶博兴-证件照.jpg";
 
 export const MemberPhotos = {
-  崔永嘉证件照,
-  卢昊楠证件照,
-  周成证件照,
-  杨光怡证件照,
-  苟凌云证件照,
-  付凯森证件照,
+  CYJ_ZJZ,
+  LHN_ZJZ,
+  ZC_SHZ,
+  ZC_ZJZ,
+  YGY_SHZ,
+  YGY_ZJZ,
+  GLY_SHZ,
+  GLY_SHZ2,
+  GLY_ZJZ,
+  FKS_ZJZ,
+  HZAR_ZJZ,
+  WMX_ZJZ,
+  WWJ_ZJZ,
+  YD_ZJZ,
+  YBX_ZJZ,
 };
