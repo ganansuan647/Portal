48338801dc97ea0818d133222612968e3847844a
ganansuan647 <1852992@tongji.edu.cn>
2024-09-16 10:44:42 +0800

    fix

--- src/images/MemberPhotos.ts
+++ src/images/MemberPhotos.ts
@@ -1,18 +1,18 @@
-const CYJ_ZJZ = "@images/成员照片/崔永嘉-证件照.jpg";
-const LHN_ZJZ = "@images/成员照片/卢昊楠-证件照.jpg";
-const ZC_SHZ = "@images/成员照片/周成-生活照.png";
-const ZC_ZJZ = "@images/成员照片/周成-证件照.png";
-const YGY_SHZ = "@images/成员照片/杨光怡-生活照.jpg";
-const YGY_ZJZ = "@images/成员照片/杨光怡-证件照.jpg";
-const GLY_SHZ = "@images/成员照片/苟凌云-生活照.jpg";
-const GLY_SHZ2 = "@images/成员照片/苟凌云-生活照2.jpg";
-const GLY_ZJZ = "@images/成员照片/苟凌云-证件照.jpg";
-const FKS_ZJZ = "@images/成员照片/付凯森-证件照.jpg";
-const HZAR_ZJZ = "@images/成员照片/黄泽安然-证件照.jpg";
-const WMX_ZJZ = "@images/成员照片/王孟欣-证件照.jpg";
-const WWJ_ZJZ = "@images/成员照片/吴文俊-证件照.jpg";
-const YD_ZJZ = "@images/成员照片/杨当-证件照.jpg";
-const YBX_ZJZ = "@images/成员照片/叶博兴-证件照.jpg";
+const CYJ_ZJZ = "@images/MemberPhotos/崔永嘉-证件照.jpg";
+const LHN_ZJZ = "@images/MemberPhotos/卢昊楠-证件照.jpg";
+const ZC_SHZ = "@images/MemberPhotos/周成-生活照.png";
+const ZC_ZJZ = "@images/MemberPhotos/周成-证件照.png";
+const YGY_SHZ = "@images/MemberPhotos/杨光怡-生活照.jpg";
+const YGY_ZJZ = "@images/MemberPhotos/杨光怡-证件照.jpg";
+const GLY_SHZ = "@images/MemberPhotos/苟凌云-生活照.jpg";
+const GLY_SHZ2 = "@images/MemberPhotos/苟凌云-生活照2.jpg";
+const GLY_ZJZ = "@images/MemberPhotos/苟凌云-证件照.jpg";
+const FKS_ZJZ = "@images/MemberPhotos/付凯森-证件照.jpg";
+const HZAR_ZJZ = "@images/MemberPhotos/黄泽安然-证件照.jpg";
+const WMX_ZJZ = "@images/MemberPhotos/王孟欣-证件照.jpg";
+const WWJ_ZJZ = "@images/MemberPhotos/吴文俊-证件照.jpg";
+const YD_ZJZ = "@images/MemberPhotos/杨当-证件照.jpg";
+const YBX_ZJZ = "@images/MemberPhotos/叶博兴-证件照.jpg";
 
 export const MemberPhotos = {
   CYJ_ZJZ,
